6829fcc1070be23c7db79365d86484f814aab980
MuratShadol <70841254+MuratShadol@users.noreply.github.com>
2020-12-21 01:21:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-k = int(input("Введите количество лет (1-9): "))
+# Скопировать из файла F1 в файл F2 строки, начиная с четвертой по порядку.
+file = open('textfile.txt')
+new_file = open('new_text_file.txt', 'a')
+n = 0
+for row in file:
+    n += 1
+ammout_of_lines = []
 
-if 1 <= k <= 4:
-    print("мне", k, "года")
-elif 5 <= k <= 9:
-    print("мне", k, "лет")
-else:
-    print("Некорректный ввод")
+for i in range(4, n + 1):
+    ammout_of_lines.append(i)
+
+for num, line in enumerate(file):
+    if num in ammout_of_lines:
+        new_file.write(line)
+
+file.close()
+new_file.close()
